23e3c4dd05e7283c3d84259be2f7226048a9077c
Mosibur Paik <51851200+mosibur1@users.noreply.github.com>
2025-10-10 21:38:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -213,7 +213,7 @@ class ClientAPI {
           return { success: false, status: error.status, error: errorMessage, data: null };
         }
         if (error.status === 400) {
-          this.log(`Invalid request for ${url}, maybe have new update from server | contact: https://t.me/airdrophuntersieutoc to get new update!`, "error");
+          this.log(`Invalid request for ${url}, maybe have new update from server | contact: https://t.me/mrptechofficial to get new update!`, "error");
           return { success: false, status: error.status, error: errorMessage, data: null };
         }
         if (error.status === 429) {
